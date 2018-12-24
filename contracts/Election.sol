9f738ab55952d35af7fdfc94b71a3b2d1dfc61c7
Yu Tong Liu <yt6liu@uwaterloo.com>
2018-12-24 17:55:43 -0500

    Adding completed UI

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,19 +7,39 @@ contract Election {
     string name;
     uint8 count;
   }
+  // Store accounts that voted
+  mapping(address => bool) public voters;
   // Store Candidates
   // Fetch Candidates
   mapping(uint8 => Candidate) public candidates;
   // Store Candidates Count
   uint8 public candidatesCount;
 
+  // voted event
+  event votedEvent (
+    uint8 indexed _candidateId
+    );
+
   constructor () public {
-    addCandidate("Candidate 1");
-    addCandidate("Candidate 2");
+    addCandidate("Shabi 1");
+    addCandidate("Shabi 2");
   }
 
   function addCandidate (string memory _name) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint8 _candidateId) public {
+    // Check if the voter has already voted
+    require(!voters[msg.sender]);
+    // Check if the vote is towards a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+    // Record that the voter has voted
+    voters[msg.sender] = true;
+    // Update vote
+    candidates[_candidateId].count ++;
+    // Trigger voted event
+    emit votedEvent(_candidateId);
   }
 }
